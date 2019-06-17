9e71469ac20946cbb20814e6c193c46f7b975743
Julien Bisconti <julien.bisconti@gmail.com>
2019-06-17 15:40:05 +0200

    Initial Commit

---
+++ modules/storage-versioned-service-account/main.tf
@@ -0,0 +1,63 @@
+#------------------------------------------------------------------------------
+# GOOGLE PROVIDER
+#------------------------------------------------------------------------------
+
+provider "google" {
+  region  = var.region
+  project = var.project_id
+}
+
+provider "google-beta" {
+  region  = var.region
+  project = var.project_id
+}
+
+#------------------------------------------------------------------------------
+# BUCKET
+#------------------------------------------------------------------------------
+
+# Generate a random id for the bucket as it must have globally unique name
+resource "random_id" "bucket_name" {
+  prefix      = var.bucket_prefix
+  byte_length = "8"
+}
+
+# Create the storage bucket
+resource "google_storage_bucket" "bucket" {
+  name     = random_id.bucket_name.hex
+  project  = var.project_id
+  location = var.location
+
+  force_destroy = var.force_destroy
+
+  storage_class = "MULTI_REGIONAL"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      num_newer_versions = var.num_newer_versions
+    }
+  }
+
+  labels = var.custom_labels
+}
+
+#------------------------------------------------------------------------------
+# GRANT ACCESS TO SERVICE ACCOUNT
+#------------------------------------------------------------------------------
+
+# Grant service account access to the storage bucket
+resource "google_storage_bucket_iam_member" "app" {
+  count  = length(var.storage_bucket_roles)
+  bucket = google_storage_bucket.bucket.name
+  role   = element(var.storage_bucket_roles, count.index)
+  member = "serviceAccount:${var.service_account_email}"
+}
+
---
+++ modules/storage-versioned-service-account/variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+
+variable "project_id" {
+}
+
+variable "bucket_prefix" {
+  default = "my-app"
+}
+
+variable "location" {
+  default = "EU"
+}
+
+variable "force_destroy" {
+  description = <<EOF
+If set to true, this will force the delete of the bucket 
+when you run terraform destroy,
+even if there is still content in it. 
+This is only meant for testing and should not be used in production.
+EOF
+
+  type = bool
+  default = false
+}
+
+variable "num_newer_versions" {
+  description = <<EOF
+The number of version to keep before permanently deleting the object.
+Usually `1` is safe. The more versions it keeps, the more storage is used.
+EOF
+default = 1
+}
+
+variable "storage_bucket_roles" {
+type = list(string)
+
+default = [
+"roles/storage.legacyBucketReader",
+"roles/storage.objectAdmin",
+]
+}
+
+variable "service_account_email" {
+description = <<EOF
+  Service account of the application that needs to be linked to the bucket storage.
+EOF
+
+}
+
+variable "custom_labels" {
+description = "A map of custom labels to apply to the resources. The key is the label name and the value is the label value."
+type = map(string)
+default = {}
+}
+
