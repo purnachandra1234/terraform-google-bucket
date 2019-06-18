b7cc8d9f337c4ef547894df2f10a1e5176f7735c
Julien Bisconti <julien.bisconti@gmail.com>
2019-06-18 16:35:41 +0200

    Formatting

--- modules/storage-versioned-service-account/variables.tf
+++ modules/storage-versioned-service-account/variables.tf
@@ -1,17 +1,36 @@
-variable "region" {
-  type    = string
-  default = "europe-west1"
-}
-
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These parameters must be supplied when consuming this module.
+# ---------------------------------------------------------------------------------------------------------------------
 variable "project_id" {
+  description = "GCP project ID"
 }
 
 variable "bucket_prefix" {
-  default = "my-app"
+  description = "Bucket name must be globally unique so this prefix will be suffixed with random string"
 }
 
 variable "location" {
-  default = "EU"
+  description = "The location of the bucket (US/EU/ASIA)"
+}
+
+variable "service_account_email" {
+  description = <<EOF
+  Service account of the application that needs to be linked to the bucket storage.
+EOF
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# Generally, these values won't need to be changed.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "storage_bucket_roles" {
+  type = list(string)
+
+  default = [
+    "roles/storage.legacyBucketReader",
+    "roles/storage.objectAdmin",
+  ]
 }
 
 variable "force_destroy" {
@@ -22,7 +41,7 @@ even if there is still content in it.
 This is only meant for testing and should not be used in production.
 EOF
 
-  type = bool
+  type    = bool
   default = false
 }
 
@@ -34,21 +53,6 @@ EOF
 default = 1
 }
 
-variable "storage_bucket_roles" {
-type = list(string)
-
-default = [
-"roles/storage.legacyBucketReader",
-"roles/storage.objectAdmin",
-]
-}
-
-variable "service_account_email" {
-description = <<EOF
-  Service account of the application that needs to be linked to the bucket storage.
-EOF
-
-}
 
 variable "custom_labels" {
 description = "A map of custom labels to apply to the resources. The key is the label name and the value is the label value."
